4d9af666caba631a8343e568c1d4aadb719a0c31
Julien <julien.dousse@gmail.com>
2022-12-03 17:22:17 +0100

    update route product

--- config/routes.php
+++ config/routes.php
@@ -5,10 +5,15 @@ require_once 'core/Router.php';
 
 use Core\Router;
 
-Router::register('/test/all', 'TestController::delete' );
+Router::register('/test/all', 'TestController::delete');
 Router::register('/login', 'UserController::login');
 Router::register('/register', 'UserController::register');
 Router::register('/task/delete', 'TaskController::delete');
 Router::register('/task/', 'TaskController::show');
 Router::register('/task/add', 'TaskController::insert');
 Router::register('/task/edit', 'TaskController::edit');
+
+// product
+Router::register('/product/add', 'ProductController::insert');
+Router::register('/product/delete', 'ProductController::delete');
+Router::register('/product/update', 'ProductController::update');
