cc98c6fbd522ffb5e174b58f2184930aac1d63d6
Julien <julien.dousse@gmail.com>
2022-12-03 15:30:43 +0100

    Getters & setters Cepage.php

--- src/models/Cepage.php
+++ src/models/Cepage.php
@@ -5,4 +5,34 @@ class Cepage
 
     private int $id;
     private string $cepage_name;
+
+    /**
+     * Get the value of id
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get the value of cepage_name
+     * @return int
+     */
+    public function getCepage_name()
+    {
+        return $this->cepage_name;
+    }
+
+    /**
+     * Set the value of cepage_name
+     *
+     * @param string $cepage_name
+     *
+     * @return void
+     */
+    public function setCepage_name(string $cepage_name): void
+    {
+        $this->cepage_name = $cepage_name;
+    }
 }
