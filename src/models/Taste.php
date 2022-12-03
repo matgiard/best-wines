03a2d0326dfc7329b61c8fefa2dd5f63d93fedbf
Julien <julien.dousse@gmail.com>
2022-12-03 16:01:13 +0100

    Getters & setters Taste.php

--- src/models/Taste.php
+++ src/models/Taste.php
@@ -5,4 +5,32 @@ class Taste
 
     private int $id;
     private string $taste_name;
+
+    /**
+     * Get the value of id
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get the value of taste_name
+     * @return string
+     */
+    public function getTaste_name(): string
+    {
+        return $this->taste_name;
+    }
+
+    /**
+     * Set the value of taste_name
+     * @param string $taste_name
+     * @return void
+     */
+    public function setTaste_name(string $taste_name): void
+    {
+        $this->taste_name = $taste_name;
+    }
 }
