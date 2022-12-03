7eab7e37295d12cddb6f95ff82c9d73caeb5bf97
Julien <julien.dousse@gmail.com>
2022-12-03 16:20:14 +0100

    Method UserController.php

---
+++ src/controllers/UserController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controllers;
+
+use Core\Controller;
+
+
+
+class UserController extends Controller
+{
+
+    public function login()
+    {
+
+        echo "ceci est la méthode login";
+    }
+
+    public function register()
+    {
+        echo "ceci est la méthode register";
+    }
+
+
+    public function logout()
+    {
+        echo "ceci est la méthode " . __FUNCTION__;
+    }
+}
