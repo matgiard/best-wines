e0d1ddb72ce67bc9f0039b9797fdc091bddc9c85
Rod97139 <fayaflame@gmail.com>
2022-12-07 09:39:44 +0100

    link layout

--- src/views/layout.php
+++ src/views/layout.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/styles/css/style.css">
     <title>Best wines</title>
 </head>
 
