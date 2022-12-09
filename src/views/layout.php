7666f6e88b1a0f3b96199b8d45320f47ab694a9e
Rod97139 <fayaflame@gmail.com>
2022-12-09 19:49:34 +0100

    login button navbar

--- src/views/layout.php
+++ src/views/layout.php
@@ -25,7 +25,10 @@
                     <a class="nav-link active" aria-current="page" href="/best-wines">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
+                    <a class="nav-link" href="/best-wines/login">Se connecter</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/best-wines/logout">Se déconnecter</a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
