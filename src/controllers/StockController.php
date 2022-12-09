e0045172f5da9396d1523b4e6ac2494e500be060
Julien <julien.dousse@gmail.com>
2022-12-09 08:46:01 +0100

    Edit fonctionnel + message s'affichant correctement

--- src/controllers/StockController.php
+++ src/controllers/StockController.php
@@ -29,56 +29,56 @@ class StockController extends Controller
      * @return void
      */
 
-     public function insert()
-     {
-         $region = new Region();
-         $regions = $region->findAll();
- 
-         $cepage = new Cepage();
-         $cepages = $cepage->findAll();
- 
-         $taste = new Taste();
-         $tastes = $taste->findAll();
- 
-         $association = new Association();
-         $associations = $association->findAll();
- 
-         $type_product = new TypeProduct();
-         $type_products = $type_product->findAll();
- 
-         $this->renderView('employe/stock/insert', compact('regions', 'cepages', 'tastes', 'associations', 'type_products'));
- 
- 
-         if (isset($_POST['submit'])) {
- 
-             $product = new Product();
- 
-             $product->setName(htmlentities($_POST['name']));
-             $product->setDescription(htmlentities($_POST['description']));
-             $product->setStock(htmlentities($_POST['stock']));
-             $product->setAlcohol_percentage(htmlentities($_POST['alcohol_percentage']));
-             $product->setId_region($_POST['id_region']);
-             $product->setId_cepage($_POST['id_cepage']);
-             $product->setId_taste($_POST['id_taste']);
-             $product->setId_association($_POST['id_association']);
-             $product->setId_type($_POST['id_type']);
-             $product->setPrice($_POST['price']);
-             // $product->setPhoto($_POST['photo']);
- 
-             $result = $product->insert();
- 
-             if ($result) {
-                 $message =  "insertion bien effectuée";
-             } else {
-                 $message =  "échec de l'insertion";
-             }
-             $this->renderView('employe/stock/insert', [
-                 'message' => $message
-             ]);
-         }
-         $this->renderView('employe/stock/insert');
-     }
- 
+    public function insert()
+    {
+        $region = new Region();
+        $regions = $region->findAll();
+
+        $cepage = new Cepage();
+        $cepages = $cepage->findAll();
+
+        $taste = new Taste();
+        $tastes = $taste->findAll();
+
+        $association = new Association();
+        $associations = $association->findAll();
+
+        $type_product = new TypeProduct();
+        $type_products = $type_product->findAll();
+
+        $this->renderView('employe/stock/insert', compact('regions', 'cepages', 'tastes', 'associations', 'type_products'));
+
+
+        if (isset($_POST['submit'])) {
+
+            $product = new Product();
+
+            $product->setName(htmlentities($_POST['name']));
+            $product->setDescription(htmlentities($_POST['description']));
+            $product->setStock(htmlentities($_POST['stock']));
+            $product->setAlcohol_percentage(htmlentities($_POST['alcohol_percentage']));
+            $product->setId_region($_POST['id_region']);
+            $product->setId_cepage($_POST['id_cepage']);
+            $product->setId_taste($_POST['id_taste']);
+            $product->setId_association($_POST['id_association']);
+            $product->setId_type($_POST['id_type']);
+            $product->setPrice($_POST['price']);
+            // $product->setPhoto($_POST['photo']);
+
+            $result = $product->insert();
+
+            if ($result) {
+                $message =  "insertion bien effectuée";
+            } else {
+                $message =  "échec de l'insertion";
+            }
+            $this->renderView('employe/stock/insert', [
+                'message' => $message
+            ]);
+        }
+        $this->renderView('employe/stock/insert');
+    }
+
     public function edit()
     {
         $id = $_GET['id'];
@@ -114,7 +114,10 @@ class StockController extends Controller
             };
             $to_edit->findOneBy(['id' => $id]);
             $edit_temp = $to_edit->findOneBy(['id' => $id]);
-            
+            $this->renderView(
+                'employe/stock/edit',
+                compact('message', 'id', 'edit_temp', 'regions', 'cepages', 'tastes', 'associations', 'type_products')
+            );
         }
         $this->renderView('employe/stock/edit', compact('id', 'edit_temp', 'regions', 'cepages', 'tastes', 'associations', 'type_products'));
     }
