093107b0d2b8e752e6d2fef5572a3265d21cc409
Rod97139 <fayaflame@gmail.com>
2022-12-11 19:10:33 +0100

    paypal

--- src/views/pay/index.php
+++ src/views/pay/index.php
@@ -1,42 +1,39 @@
+<!DOCTYPE html>
 <html>
-	<head>
-	  <title>Page de paiement</title>
-	  <meta charset="utf-8">
-	  <script src="https://www.paypalobjects.com/api/checkout.js"></script>
-	</head>
-	<body>
-	  <div id="bouton-paypal"></div>
-	  <script>
-	    paypal.Button.render({
-	      env: 'sandbox', // Ou 'production',
-	      commit: true, // Affiche le bouton  "Payer maintenant"
-	      style: {
-	        color: 'gold', // ou 'blue', 'silver', 'black'
-	        size: 'responsive' // ou 'small', 'medium', 'large'
-	        // Autres options de style disponibles ici : https://developer.paypal.com/docs/integration/direct/express-checkout/integration-jsv4/customize-button/
-	      },
-	      payment: function(data, actions) {
-	        /* 
-	         * Création du paiement
-	         */
-	        console.log('paiement créé');
-	      },
-	      onAuthorize: function(data, actions) {
-	        /* 
-	         * Exécution du paiement 
-	         */
-	      },
-	      onCancel: function(data, actions) {
-	        /* 
-	         * L'acheteur a annulé le paiement
-	         */
-	      },
-	      onError: function(err) {
-	        /* 
-	         * Une erreur est survenue durant le paiement 
-	         */
-	      }
-	    }, '#bouton-paypal');
-	  </script>
-	</body>
-	</html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+  <!-- Replace "test" with your own sandbox Business account app client ID -->
+  <script src="https://www.paypal.com/sdk/js?client-id=test&currency=USD"></script>
+  <!-- Set up a container element for the button -->
+  <div id="paypal-button-container"></div>
+  <script>
+    paypal.Buttons({
+      // Sets up the transaction when a payment button is clicked
+      createOrder: (data, actions) => {
+        return actions.order.create({
+          purchase_units: [{
+            amount: {
+              value: '77.44' // Can also reference a variable or function
+            }
+          }]
+        });
+      },
+      // Finalize the transaction after payer approval
+      onApprove: (data, actions) => {
+        return actions.order.capture().then(function(orderData) {
+          // Successful capture! For dev/demo purposes:
+          console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
+          const transaction = orderData.purchase_units[0].payments.captures[0];
+          alert(`Transaction ${transaction.status}: ${transaction.id}\n\nSee console for all available details`);
+          // When ready to go live, remove the alert and show a success message within this page. For example:
+          // const element = document.getElementById('paypal-button-container');
+          // element.innerHTML = '<h3>Thank you for your payment!</h3>';
+          // Or go to another URL:  actions.redirect('thank_you.html');
+        });
+      }
+    }).render('#paypal-button-container');
+  </script>
+</body>
+</html>
