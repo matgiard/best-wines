0f9432522d16fbe7c3beb3addfce9ab86bbfb19a
Julien <julien.dousse@gmail.com>
2022-12-11 16:28:29 +0100

    ajout bouton article

--- src/views/blog/index.php
+++ src/views/blog/index.php
@@ -7,6 +7,7 @@
 <?php endif; ?>
 
 <h1>Nos Articles</h1>
+<a href="<?= BASE_DIR ?>/blog/insert" class="btn btn-warning">Ajouter un article</a>
 <?php foreach ($articles as $article) : ?>
     <div class="border">
         <div class="col-md-4">
@@ -15,7 +16,7 @@
         <h2><?= $article['title'] ?></h2>
         <div><?= substr($article['content'], 0, 300) ?></div>
         <a href="<?= BASE_DIR ?>/blog/details?id=<?= $article['id'] ?>">Voir plus</a>
-        < <small>Ajouté le <?= $article['date'] ?></small>
-            <a href="<?= BASE_DIR ?>/blog/edit?id=<?= $article['id'] ?>" class="btn btn-warning">Modifier</a>
+        <small>Ajouté le <?= $article['date'] ?></small>
+        <a href="<?= BASE_DIR ?>/blog/edit?id=<?= $article['id'] ?>" class="btn btn-warning">Modifier</a>
     </div>
 <?php endforeach ?>
