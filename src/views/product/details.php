ec9c34d9b2690d60d1fe59c5a08bc83047dcb337
Julien <julien.dousse@gmail.com>
2022-12-14 11:34:10 +0100

    details.php updated

--- src/views/product/details.php
+++ src/views/product/details.php
@@ -1,7 +1,7 @@
 <div class="container" id="product-section">
     <div class="row">
         <div class="col-md-6">
-            <img src="<?= BASE_DIR ?>/uploads/<?= $products['photo']; ?>" alt="<?= $products['name'] ?>">
+            <img src="<?= BASE_DIR ?>/uploads/vins/<?= $products['photo']; ?>" alt="<?= $products['name'] ?>">
 
         </div>
         <div class="col-md-6">
@@ -13,40 +13,38 @@
             <h2 class="product-price"><?= $products['price'] ?>€</h2>
             <h2 class="product-price"><?= $products['region_name'] ?></h2>
             <section>
-                <form action="/best-wines/cart/add?name=<?=$products['name']?>" method="POST">
+                <form action="/best-wines/cart/add?name=<?= $products['name'] ?>" method="POST">
                     <p class="qty">
                         <label for="qty">Quantity:</label>
-                       
+
                         <input type="number" name="qty" id="qty" min="1" max="30" step="1" value="1">
-                        
+
                         <button type="input" class="btn btn-success">Ajouter au panier</button>
                     </p>
-                </form>			
+                </form>
             </section>
-            
+
         </div>
     </div>
 </div>
 
 <h4>Autres Vins conseillés</h4>
-        <div class="container mt-5">
-            <div class="row row-cols-1 row-cols-md-4 g-4">
-                <div class="card mb-3" style="max-width: 540px;">
-                    <div class="row g-0">
-                        <div class="col-md-4"></div>
-                        <div class="col-md-8">
-                            <h5 class="card-title"></h5>
-                            <div class="col-md-4">
-                            <img src="<?= BASE_DIR ?>/uploads/<?= $lastWhiteWine['photo'] ?>" alt="<?= $lastWhiteWine['name'] ?>" srcset="" class="img-fluid rounded-start">
-                </div>
-                            <p class="card-title"><?= $lastWhiteWine['name'] ?></p>
-                            <p class="card-text"><?= $lastWhiteWine['price'] ?>€</p>
-                            <p class="card-text"></p>
-                            <a href="#" class="btn btn-primary">Détails</a>
-                        </div>
+<div class="container mt-5">
+    <div class="row row-cols-1 row-cols-md-4 g-4">
+        <div class="card mb-3" style="max-width: 540px;">
+            <div class="row g-0">
+                <div class="col-md-4"></div>
+                <div class="col-md-8">
+                    <h5 class="card-title"></h5>
+                    <div class="col-md-4">
+                        <img src="<?= BASE_DIR ?>/uploads/<?= $lastWhiteWine['photo'] ?>" alt="<?= $lastWhiteWine['name'] ?>" srcset="" class="img-fluid rounded-start">
                     </div>
+                    <p class="card-title"><?= $lastWhiteWine['name'] ?></p>
+                    <p class="card-text"><?= $lastWhiteWine['price'] ?>€</p>
+                    <p class="card-text"></p>
+                    <a href="#" class="btn btn-primary">Détails</a>
                 </div>
             </div>
         </div>
-
-
+    </div>
+</div>
